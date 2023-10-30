54ef2bc0e8679f151df4087ccd35a42f6a5e0b6f
322332 <gokhan.demirtas@msn.com>
2023-10-30 18:56:09 +0300

    Change platform npm adress

--- src/app.jsx
+++ src/app.jsx
@@ -5,9 +5,6 @@ import theme from "./config/theme";
 import routes from "./config/routes";
 import config from "./config/config";
 
-//import Plat2 from "../../../Documents/platform/src/Platform";
-//import Plat2 from "gokhan-platform/src/Platform";
-
 export function App() {
   return <Platform theme={theme} routes={routes} />;
 }
